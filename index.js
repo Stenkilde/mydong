87dbb1d67485a9a4d932cf53284929c85256d415
Stenkilde <hello@stenkil.de>
2016-12-12 16:58:47 +0100

    Made a more functional CLI

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
 #!/usr/bin/env node
 const prompt = require('prompt');
 const ncp = require('copy-paste');
-
-prompt.start();
-
-	prompt.message = 'Hello sir';
-
-	prompt.get({
-	    properties: {
-	      	dong: {
-	        	description: 'Which dong do you want today?'
-	  		}
-	    }
-	  }, function (err, result) {
-	    	let filteredDong = dongers.filter(dong => dong.name === result.dong);
-	    	let escapedDong = filteredDong.shift().code;
-	    	ncp.copy(escapedDong, function() {
-	    		console.log('We saved him to your clipboard!')
-	    	})
-			console.log(escapedDong);
-	});
+const program = require('commander');
+const chalk = require('chalk');
 
 let dongers = [
 	{
@@ -33,3 +16,38 @@ let dongers = [
 		code: '¯\\_(ツ)_/¯'
 	}
 ];
+
+program
+  .option('-d, --dong [type]', 'Add the specified type of dong [marble]', 'marble')
+  .parse(process.argv);
+
+console.log(chalk.bold.green('You asked for the following dong: ', program.dong));
+
+let argumentDong = dongers.filter(dong => dong.name === program.dong);
+
+if (argumentDong.length > 0) {
+	ncp.copy(argumentDong[0].code, function() {
+		console.log(chalk.bold.green('We saved him to your clipboard!'));
+		console.log(chalk.bold.cyan(argumentDong[0].code));
+	})
+	return;
+} else {
+	prompt.start();
+
+		prompt.message = 'Hello sir';
+
+		prompt.get({
+		    properties: {
+		      	dong: {
+		        	description: 'Which dong do you want today?'
+		  		}
+		    }
+		  }, function (err, result) {
+		    	let filteredDong = dongers.filter(dong => dong.name === result.dong);
+		    	let escapedDong = filteredDong.shift().code;
+		    	ncp.copy(escapedDong, function() {
+		    		console.log(chalk.bold.cyan('We saved him to your clipboard!'));
+		    	})
+				console.log(escapedDong);
+		});
+}
