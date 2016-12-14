717b1ef480af6e6bfeb3c7766db8c06c6bfd2345
Oliver Stenkilde <olm@tricom.dk>
2016-12-14 13:58:45 +0100

    Added list command

--- index.js
+++ index.js
@@ -33,36 +33,45 @@ let dongers = [
 ];
 
 program
-  .option('-d, --dong [type]', 'Add the specified type of dong [marble]', 'marble')
-  .parse(process.argv);
+	.option('-d, --dong [type]', 'Add the specified type of dong [marble]', 'marble')
+	.option('-l, --list', 'List of all the dongs possible to use')
+	.parse(process.argv);
 
-console.log(chalk.bold.green('You asked for the following dong: ', program.dong));
+if (process.argv[2] === '-l') {
+    let allDongs = dongers.map(function(dong) {
+       console.log(dong.name, dong.code);
+    });
+}
 
-let argumentDong = dongers.filter(dong => dong.name === program.dong);
-
-if (argumentDong.length > 0) {
-	ncp.copy(argumentDong[0].code, function() {
-		console.log(chalk.bold.green('We saved him to your clipboard!'));
-		console.log(chalk.bold.cyan(argumentDong[0].code));
-	})
-	return;
-} else {
-	prompt.start();
-
-	prompt.message = 'Hello sir';
-
-	prompt.get({
-	    properties: {
-	      	dong: {
-	        	description: 'Which dong do you want today?'
-	  		}
-	    }
-	  }, function (err, result) {
-	    	let filteredDong = dongers.filter(dong => dong.name === result.dong);
-	    	let escapedDong = filteredDong.shift().code;
-	    	ncp.copy(escapedDong, function() {
-	    		console.log(chalk.bold.cyan('We saved him to your clipboard!'));
-	    	})
-			console.log(escapedDong);
-	});
+if (process.argv[2] === '-d') {
+    console.log(chalk.bold.green('You asked for the following dong: ', program.dong));
+    
+    let argumentDong = dongers.filter(dong => dong.name === program.dong);
+    
+    if (argumentDong.length > 0) {
+        ncp.copy(argumentDong[0].code, function() {
+            console.log(chalk.bold.green('We saved him to your clipboard!'));
+            console.log(chalk.bold.cyan(argumentDong[0].code));
+        })
+        return;
+    } else {
+        prompt.start();
+        
+        prompt.message = 'Hello sir';
+        
+        prompt.get({
+            properties: {
+                dong: {
+                    description: 'Which dong do you want today?'
+                }
+            }
+        }, function (err, result) {
+            let filteredDong = dongers.filter(dong => dong.name === result.dong);
+            let escapedDong = filteredDong.shift().code;
+            ncp.copy(escapedDong, function() {
+                console.log(chalk.bold.cyan('We saved him to your clipboard!'));
+            })
+            console.log(escapedDong);
+        });
+    }
 }
